fb1725773666e88660b5c2c8d5ba14e54833c02d
guilherme-carasek <guilhermemcarasek@outlook.com>
2024-11-25 23:29:25 +0000

    added to ALGPL03, added to WEBPL03

--- sftdev/ALG/PL03/src/Main.java
+++ sftdev/ALG/PL03/src/Main.java
@@ -14,15 +14,16 @@ public class Main {
             if (opcao == 1) {
                 Calculadora();
             }
+            else if (opcao == 2){
+                Lista();
+            }
         }
         System.out.println("Goodbye");
-
-
     }
     private static void Calculadora() {
         int opcao = 1;
         while (opcao != 0) {
-            System.out.println("Você está em: Calculadora:");
+            System.out.println("Você está em 1-Calculadora:");
             System.out.println("1-Soma");
             System.out.println("2-subtração");
             System.out.println("0-Voltar");
@@ -36,13 +37,36 @@ public class Main {
                     Subtracao();
             }
         }
-
     }
-
     private static void Soma() {
+        System.out.println("Insira um número");
+        double primeiroNumero = in.nextDouble();
+        System.out.println("Insira outro número");
+        double segundoNumero = in.nextDouble();
+        System.out.println("A soma entre "+primeiroNumero+" e "+segundoNumero+" é "+(primeiroNumero+segundoNumero));
     }
-
     private static void Subtracao() {
+        System.out.println("Insira um número");
+        double primeiroNumero = in.nextDouble();
+        System.out.println("Insira outro número");
+        double segundoNumero = in.nextDouble();
+        System.out.println(primeiroNumero+" - "+segundoNumero+" = "+(primeiroNumero-segundoNumero));
     }
-
+    private static void Lista(){
+        System.out.println("Aplicação 2- Lista:");
+        System.out.println("Insira um número inteiro");
+        int primeiroNumero = in.nextInt();
+        System.out.println("Insira outro número inteiro");
+        int segundoNumero = in.nextInt();
+        if (primeiroNumero>segundoNumero){
+            int temporario=primeiroNumero;
+            primeiroNumero=segundoNumero;
+            segundoNumero=temporario;
+        }
+        while (primeiroNumero<=segundoNumero){
+            System.out.print(primeiroNumero+",");
+            primeiroNumero++;
+        }
+        System.out.println(" ");
+    }
 }
