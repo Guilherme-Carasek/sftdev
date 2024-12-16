82a913c1d336001d28df4c47662018952cbe2a2e
guilherme-carasek <guilhermemcarasek@outlook.com>
2024-12-16 01:40:25 +0000

    webc

--- sftdev/WEBC/primeiroProjeto/js/exemplo.js
+++ sftdev/WEBC/primeiroProjeto/js/exemplo.js
@@ -1,18 +1,21 @@
+const prompt = require('prompt-sync')()
 
 
-function generateRandomIntegers(parametro){
-    for (i=0;i<parametro;i++){
-        console.log((Math.random()*100).toFixed(0))
+
+let opcao = 0
+
+while (opcao != 6){
+    console.log("1.Ler valores")
+    console.log("2.Ver valores")
+    console.log("3.Média")
+    console.log("4.Máximo")
+    console.log("5.Mínimo")
+    console.log("6.Sair")
+    opcao = prompt()
+    console.log(`Clicou na opção ${opcao}`)
+    
+    switch(opcao){
+        case 1: console.log(`Clicou na opção ${opcao}`)
     }
+
 }
-
-function valorEntreDois(x,y){
-    if (x<y){
-        console.log(((Math.random()*(y-x))+x).toFixed(0))
-    }
-    else{
-        console.log((Math.random()*(x-y))+y)
-    }
-}
-
-valorEntreDois(1,5)
