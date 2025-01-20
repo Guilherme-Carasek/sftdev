60220f0c28e2fa6725c767b502830153495b9bab
Guilherme Carasek <guilhermemcarasek@outlook.com>
2025-01-20 16:46:48 +0000

    PHPaula1end

--- sftdev/PHP/Aula1/ServerSideSD/routes/web.php
+++ sftdev/PHP/Aula1/ServerSideSD/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/users', function () {
+    return view('users.all_users');
+})->name('users.show');
+
+Route::get('/add-users', function () {
+    return view('users.add_users');
+})->name('users.add');
+
+Route::fallback(function(){
+    return "<a href=".route('home')."><h1>Estás perdido volte aqui</h1></a>";
+});
+
+Route::get('/hello/{name}', function ($name) {
+    return '<h1>Hello '.$name.'</h1>';
+});
