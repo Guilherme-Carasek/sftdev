dcbb02914094889236f3d8aac68753320400d820
Guilherme Carasek <guilhermemcarasek@outlook.com>
2025-01-29 16:56:42 +0000

    MOBaula1

---
+++ sftdev/MOB/app/src/main/java/com/example/a01_primeiraapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a01_primeiraapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a01_primeiraapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonConverterDol.setOnClickListener {
+            val valorEur: Double = binding.editValorEur.text.toString().toDouble()
+            val convRate: Double = 0.86
+            var valorDol = valorEur*convRate
+            binding.textResultado.text = "O valor em dólares é: \n $valorDol\$"
+        }
+    }
+}
