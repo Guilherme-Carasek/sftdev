e1fd61ca704937c1d59a2717462109eb19f16e59
Guilherme Carasek <guilhermemcarasek@outlook.com>
2025-02-21 16:52:07 +0000

    mob

--- sftdev/MOB/MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
+++ sftdev/MOB/MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mvvm
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvm.databinding.ActivityMainBinding
 
@@ -18,6 +20,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.welcome().observe(this, {
+            binding.textWelcome.text = it
+        })
+
+        viewModel.login().observe(this, Observer {
+            if(it){
+                Toast.makeText(this, "Login OK", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Login Errado", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.buttonLogin.setOnClickListener{
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+
+            viewModel.doLogin(email, password)
+        }
 
     }
 }
