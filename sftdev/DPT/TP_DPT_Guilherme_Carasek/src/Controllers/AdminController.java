538f1d92092a8439f5c4654a4935addf005a10f9
Guilherme Carasek <guilhermemcarasek@outlook.com>
2025-03-12 16:44:57 +0000

    tp dpt incomplete

--- sftdev/DPT/TP_DPT_Guilherme_Carasek/src/Controllers/AdminController.java
+++ sftdev/DPT/TP_DPT_Guilherme_Carasek/src/Controllers/AdminController.java
@@ -1,10 +1,12 @@
 package Controllers;
 
+import Domain.Attraction;
 import Model.AttractionsRepository;
 import Model.CostsRepository;
 import Model.SalesRepository;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class AdminController {
     AttractionsRepository attractionsRepository;
@@ -17,6 +19,29 @@ public class AdminController {
         this.costsRepository = new CostsRepository();
     }
 
+    public double getSalesTotal() {
+        return 0;
+    }
 
+    public double getProfitsTotal() {
+        return 0;
+    }
+
+    /**
+     * Gets sales and profits, organized by month
+     * @return ArrayList of table values, each an array of: [0] = month, [1] = total sales for the month, [2] = total profit for the month
+     */
+    public ArrayList<String[]> getSalesAndProfitsTable () {
+        ArrayList<String[]> tableValues = new ArrayList<>();
+        //organize each month into an array of strings, then add to ArrayList
+
+        return tableValues;
+    }
+
+    public Attraction getAdultsFavoriteAttraction() {
+        Attraction favoriteAttractionForAdults;
+
+        return favoriteAttractionForAdults;
+    }
 
 }
--- sftdev/DPT/TP_DPT_Guilherme_Carasek/src/Model/SalesRepository.java
+++ sftdev/DPT/TP_DPT_Guilherme_Carasek/src/Model/SalesRepository.java
@@ -1,9 +1,19 @@
 package Model;
 
 import Domain.Sale;
+import Tools.CSVReader;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class SalesRepository {
-    ArrayList<Sale> sales
+    ArrayList<Sale> salesList;
+
+    public SalesRepository() throws FileNotFoundException {
+        this.salesList = CSVReader.readSalesFileToArray("files/Cesaeland_vendas");
+    }
+
+    public ArrayList<Sale> getSalesList() {
+        return salesList;
+    }
 }
